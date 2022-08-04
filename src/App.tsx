670a08b6a7c331f75865c86f6e136c2b4e5e4a9c
Viktor Riabokon <vikrib9@gmail.com>
2022-08-04 22:02:41 +0300

    result

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { Registration } from './components/Registration/Registration';
+import { Login } from './components/Login/Login';
+import { Home } from './components/Home/Home';
 
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
-
-export const App: React.FC = () => {
+function App() {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="auth/register" element={<Registration />} />
+        <Route path="auth/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../../api';
+import './Home.scss';
+import '../Registration/Registration.scss';
+import confeties from '../../img/Decor.png';
+import people from '../../img/Vector (1).png';
+
+export const Home: React.FC = () => {
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleOut = async () => {
+    const response = await logout();
+
+    if (!response.ok) {
+      setError(true);
+    } else {
+      setError(false);
+      navigate('/auth/login', { replace: true });
+    }
+  };
+
+  return (
+    <div className="home">
+      <div className="header home__header">
+        <div className="header__company">InCode</div>
+        <div className="header__spec">Finance</div>
+      </div>
+      <div className="home__content">
+        <div className="home__congrats">
+          congratulations
+          <img
+            src={confeties}
+            alt="Confeties"
+            className="home__congrats--img"
+          />
+        </div>
+        <div className="home__text">
+          Now you are on the main page.
+          Soon we will provide you with detailed feedback on the result of your work
+        </div>
+        <button
+          className="home__logout"
+          type="button"
+          onClick={handleOut}
+        >
+          See you
+        </button>
+        <img
+          src={people}
+          alt="People"
+          className="home__people"
+        />
+      </div>
+
+      {error && <div className="home__error">You need to sign in</div>}
+    </div>
+  );
+};
